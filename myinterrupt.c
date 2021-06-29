ab55a633b438d7b28bad36c1d289028b7baeb430
xiecang <xiecang2014@xiaochuankeji.cn>
2021-06-29 11:38:38 +0800

    init

--- myinterrupt.c
+++ myinterrupt.c
@@ -2,71 +2,43 @@
  *  linux/mykernel/myinterrupt.c
  *
  *  Kernel internal my_timer_handler
- *  Change IA32 to x86-64 arch, 2020/4/26
  *
- *  Copyright (C) 2013, 2020  Mengning
+ *  Copyright (C) 2013  Mengning
  *
  */
-#include <linux/types.h>
-#include <linux/string.h>
-#include <linux/ctype.h>
-#include <linux/tty.h>
-#include <linux/vmalloc.h>
+#include <linux/kernel_stat.h>
+#include <linux/export.h>
+#include <linux/interrupt.h>
+#include <linux/percpu.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+#include <linux/swap.h>
+#include <linux/pid_namespace.h>
+#include <linux/notifier.h>
+#include <linux/thread_info.h>
+#include <linux/time.h>
+#include <linux/jiffies.h>
+#include <linux/posix-timers.h>
+#include <linux/cpu.h>
+#include <linux/syscalls.h>
+#include <linux/delay.h>
+#include <linux/tick.h>
+#include <linux/kallsyms.h>
+#include <linux/irq_work.h>
+#include <linux/sched.h>
+#include <linux/sched/sysctl.h>
+#include <linux/slab.h>
 
-#include "mypcb.h"
-
-extern tPCB task[MAX_TASK_NUM];
-extern tPCB * my_current_task;
-extern volatile int my_need_sched;
-volatile int time_count = 0;
+#include <asm/uaccess.h>
+#include <asm/unistd.h>
+#include <asm/div64.h>
+#include <asm/timex.h>
+#include <asm/io.h>
 
 /*
  * Called by timer interrupt.
- * it runs in the name of current running process,
- * so it use kernel stack of current running process
  */
 void my_timer_handler(void)
 {
-    if(time_count%1000 == 0 && my_need_sched != 1)
-    {
-        printk(KERN_NOTICE ">>>my_timer_handler here<<<\n");
-        my_need_sched = 1;
-    } 
-    time_count ++ ;  
-    return;  	
-}
-
-void my_schedule(void)
-{
-    tPCB * next;
-    tPCB * prev;
-
-    if(my_current_task == NULL 
-        || my_current_task->next == NULL)
-    {
-    	return;
-    }
-    printk(KERN_NOTICE ">>>my_schedule<<<\n");
-    /* schedule */
-    next = my_current_task->next;
-    prev = my_current_task;
-    if(next->state == 0)/* -1 unrunnable, 0 runnable, >0 stopped */
-    {        
-    	my_current_task = next; 
-    	printk(KERN_NOTICE ">>>switch %d to %d<<<\n",prev->pid,next->pid);  
-    	/* switch to next process */
-    	asm volatile(	
-        	"pushq %%rbp\n\t" 	    /* save rbp of prev */
-        	"movq %%rsp,%0\n\t" 	/* save rsp of prev */
-        	"movq %2,%%rsp\n\t"     /* restore  rsp of next */
-        	"movq $1f,%1\n\t"       /* save rip of prev */	
-        	"pushq %3\n\t" 
-        	"ret\n\t" 	            /* restore  rip of next */
-        	"1:\t"                  /* next process start here */
-        	"popq %%rbp\n\t"
-        	: "=m" (prev->thread.sp),"=m" (prev->thread.ip)
-        	: "m" (next->thread.sp),"m" (next->thread.ip)
-    	); 
-    }  
-    return;	
+	pr_notice("\n>>>>>>>>>>>>>>>>>my_timer_handler here<<<<<<<<<<<<<<<<<<\n\n");
 }
--- mymain.c
+++ mymain.c
@@ -2,75 +2,90 @@
  *  linux/mykernel/mymain.c
  *
  *  Kernel internal my_start_kernel
- *  Change IA32 to x86-64 arch, 2020/4/26
  *
- *  Copyright (C) 2013, 2020  Mengning
- *  
+ *  Copyright (C) 2013  Mengning
+ *
  */
 #include <linux/types.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/stackprotector.h>
 #include <linux/string.h>
 #include <linux/ctype.h>
+#include <linux/delay.h>
+#include <linux/ioport.h>
+#include <linux/init.h>
+#include <linux/initrd.h>
+#include <linux/acpi.h>
 #include <linux/tty.h>
+#include <linux/percpu.h>
+#include <linux/kmod.h>
 #include <linux/vmalloc.h>
+#include <linux/kernel_stat.h>
+#include <linux/start_kernel.h>
+#include <linux/security.h>
+#include <linux/smp.h>
+#include <linux/profile.h>
+#include <linux/rcupdate.h>
+#include <linux/moduleparam.h>
+#include <linux/kallsyms.h>
+#include <linux/writeback.h>
+#include <linux/cpu.h>
+#include <linux/cpuset.h>
+#include <linux/cgroup.h>
+#include <linux/efi.h>
+#include <linux/tick.h>
+#include <linux/interrupt.h>
+#include <linux/taskstats_kern.h>
+#include <linux/delayacct.h>
+#include <linux/unistd.h>
+#include <linux/rmap.h>
+#include <linux/mempolicy.h>
+#include <linux/key.h>
+#include <linux/buffer_head.h>
+#include <linux/debug_locks.h>
+#include <linux/debugobjects.h>
+#include <linux/lockdep.h>
+#include <linux/kmemleak.h>
+#include <linux/pid_namespace.h>
+#include <linux/device.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/signal.h>
+#include <linux/idr.h>
+#include <linux/kgdb.h>
+#include <linux/ftrace.h>
+#include <linux/async.h>
+#include <linux/sfi.h>
+#include <linux/shmem_fs.h>
+#include <linux/slab.h>
+#include <linux/perf_event.h>
+#include <linux/file.h>
+#include <linux/ptrace.h>
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
 
+#include <asm/io.h>
+#include <asm/bugs.h>
+#include <asm/setup.h>
+#include <asm/sections.h>
+#include <asm/cacheflush.h>
 
-#include "mypcb.h"
-
-tPCB task[MAX_TASK_NUM];
-tPCB * my_current_task = NULL;
-volatile int my_need_sched = 0;
-
-void my_process(void);
+#ifdef CONFIG_X86_LOCAL_APIC
+#include <asm/smp.h>
+#endif
 
 
 void __init my_start_kernel(void)
 {
-    int pid = 0;
-    int i;
-    /* Initialize process 0*/
-    task[pid].pid = pid;
-    task[pid].state = 0;/* -1 unrunnable, 0 runnable, >0 stopped */
-    task[pid].task_entry = task[pid].thread.ip = (unsigned long)my_process;
-    task[pid].thread.sp = (unsigned long)&task[pid].stack[KERNEL_STACK_SIZE-1];
-    task[pid].next = &task[pid];
-    /*fork more process */
-    for(i=1;i<MAX_TASK_NUM;i++)
-    {
-        memcpy(&task[i],&task[0],sizeof(tPCB));
-        task[i].pid = i;
-	    task[i].thread.sp = (unsigned long)(&task[i].stack[KERNEL_STACK_SIZE-1]);
-        task[i].next = task[i-1].next;
-        task[i-1].next = &task[i];
-    }
-    /* start process 0 by task[0] */
-    pid = 0;
-    my_current_task = &task[pid];
-	asm volatile(
-    	"movq %1,%%rsp\n\t" 	/* set task[pid].thread.sp to rsp */
-    	"pushq %1\n\t" 	        /* push rbp */
-    	"pushq %0\n\t" 	        /* push task[pid].thread.ip */
-    	"ret\n\t" 	            /* pop task[pid].thread.ip to rip */
-    	: 
-    	: "c" (task[pid].thread.ip),"d" (task[pid].thread.sp)	/* input c or d mean %ecx/%edx*/
-	);
-} 
-
-int i = 0;
-
-void my_process(void)
-{    
+    int i = 0;
     while(1)
     {
         i++;
-        if(i%10000000 == 0)
-        {
-            printk(KERN_NOTICE "this is process %d -\n",my_current_task->pid);
-            if(my_need_sched == 1)
-            {
-                my_need_sched = 0;
-        	    my_schedule();
-        	}
-        	printk(KERN_NOTICE "this is process %d +\n",my_current_task->pid);
-        }     
+        if(i%100000 == 0)
+            pr_notice("my_start_kernel here  %d \n",i);
+            
     }
 }
